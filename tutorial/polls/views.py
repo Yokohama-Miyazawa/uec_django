9af9b769c8d475a541f819ea47f79c6881bc1ad7
Osamu Miyazawa <yokohama@Osamu-no-MacBook-Air.local>
2017-01-27 20:43:20 +0900

    vote関数の中身を作成

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -20,4 +20,15 @@ def detail(request, pk):
 
 
 def vote(request, pk):
-    pass
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+         'question': question,
+         'error_message': "選択肢が何も選ばれていません。",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('index')
