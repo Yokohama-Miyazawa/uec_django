272f5edbf664949b9f74deb4e8c0808210f03ce8
Osamu Miyazawa <yokohama@Osamu-no-MacBook-Air.local>
2017-01-27 14:28:16 +0900

    /polls/admin.py に ChoiceInline, QuestionAdmin を追加

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
