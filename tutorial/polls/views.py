2d3e75375bae5818b3257dee9967e401f3d4a18d
Osamu Miyazawa <yokohama@Osamu-no-MacBook-Air.local>
2017-01-27 16:25:50 +0900

    index関数にmark_safe関数を使用。これでHTMLタグはエスケープされない

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.utils.html import mark_safe
 
 
 def index(request):
     return render(request, 'polls/index.html', {
         'hoge': 'Tokyo',
         'fuga': 'Chofu<br><br><br>UEC',
+        'piyo': mark_safe('Chofu<br><br><br>UEC'),
     })
