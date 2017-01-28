e1788c44f9553d7eb258946781adf82fc8e14e7b
Osamu Miyazawa <yokohama@Osamu-no-MacBook-Air.local>
2017-01-28 13:53:27 +0900

    /polls/urls.py からvote関数の呼び出しを削除

--- tutorial/polls/urls.py
+++ tutorial/polls/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'(?P<pk>\d+)/$', views.detail, name='detail'),
-    url(r'(?P<pk>\d+)/vote$', views.vote, name='vote'),
     url(r'(?P<pk>\d+)/results$', views.results, name='results'),
     url(r'^form$', views.form_test),
 ]
