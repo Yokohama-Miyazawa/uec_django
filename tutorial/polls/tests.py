45e5ed66e31ea8c55c2d38fd255afb8220266d18
Osamu Miyazawa <yokohama@Osamu-no-MacBook-Air.local>
2017-03-17 01:56:58 +0900

    2回目のテストを実行。
    func_fugaが1を返すか確認するテストを追加。
    これは0を返す関数なので失敗する。

--- tutorial/polls/tests.py
+++ tutorial/polls/tests.py
@@ -5,6 +5,11 @@ def func_hoge():  # テスト対象の関数その1
     return 1
 
 
+def func_fuga():  # テスト対象の関数その2
+    return 0
+
+
 class PollsTest(TestCase):  # TestCaseの子クラス
     def test_func(self):  # テスト用のメソッド
         self.assertEqual(1, func_hoge())  # func_hogeは1を返すか確認
+        self.assertEqual(1, func_fuga())  # func_fugaは1を返すか確認
