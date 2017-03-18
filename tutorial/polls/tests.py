8dfed61a35cf45a91959d9ee8b602fce387998cf
Osamu Miyazawa <yokohama@Osamu-no-MacBook-Air.local>
2017-03-19 03:02:40 +0900

    /polls/tests.pyを変更。関数func_piyoを追加しその返り値と1を比較。

--- tutorial/polls/tests.py
+++ tutorial/polls/tests.py
@@ -9,7 +9,12 @@ def func_fuga():  # テスト対象の関数その2
     return 0
 
 
+def func_piyo():  # テスト対象の関数その3
+    return 1 == 1
+
+
 class PollsTest(TestCase):  # TestCaseの子クラス
     def test_func(self):  # テスト用のメソッド
         self.assertEqual(1, func_hoge())  # func_hogeは1を返すか確認
         self.assertNotEqual(1, func_fuga())  # func_fugaは1以外を返すか確認
+        self.assertEqual(1, func_piyo())  # func_piyoはTrueを返すか確認
